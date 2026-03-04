57017921cb9da368e9abeba40605ace8ce20087d
Tahir <30388791+mtahirofficial@users.noreply.github.com>
2026-03-05 01:11:32 +0500

    update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api"; // update if needed
+const BASE_URL = "/api"; // update if needed
 
 const api = axios.create({
   baseURL: BASE_URL,
